71c942a70992f0943aa90da4ffb82d6d3c5b67ff
coddingp <theodore1seriakov@gmail.com>
2021-12-14 19:42:39 +0600

    Delete ExchangeDollar.java

---
+++ src/circles/Main.java
@@ -0,0 +1,19 @@
+package circles;
+
+import javax.swing.*;
+import java.awt.*;
+
+//import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+//        Circle redCircle = new Circle(18, "Red");
+//
+//        System.out.println(redCircle.toString());
+//        Circle blueCircle = new Circle(13, "Blue");
+//        System.out.println(blueCircle.toString());
+
+        SetRadius setRadius= new SetRadius("RED",654);
+        SetRadius.main(args);
+    }
+}
+
--- src/itHomeWork4Waiting for zero/src/com/company/Main.java
+++ src/itHomeWork4Waiting for zero/src/com/company/Main.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-        int temp = 0;
-        int i = 0;
-        double dev = 0;
-        while (true) {
-            i++;
-            System.out.println("Введите число: ");
-            Scanner scn = new Scanner(System.in);
-            int scnNew1 = scn.nextInt();
-            if (scnNew1 != 0) {
-                temp += scnNew1;
-                dev += scnNew1;
-            } else {
-                System.out.println("Количество введенных чисел: " + (i));
-                System.out.println("Сумма: " + dev);
-                System.out.println("Среднее арифметическое: " + (dev / i));
-                break;
-            }
-        }
-    }
-}
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        int temp = 0;
+//        int i = 0;
+//        double dev = 0;
+//        while (true) {
+//            i++;
+//            System.out.println("Введите число: ");
+//            Scanner scn = new Scanner(System.in);
+//            int scnNew1 = scn.nextInt();
+//            if (scnNew1 != 0) {
+//                temp += scnNew1;
+//                dev += scnNew1;
+//            } else {
+//                System.out.println("Количество введенных чисел: " + (i));
+//                System.out.println("Сумма: " + dev);
+//                System.out.println("Среднее арифметическое: " + (dev / i));
+//                break;
+//            }
+//        }
+//    }
+//}
