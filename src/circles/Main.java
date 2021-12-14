93bcf21c6a17a5845720305ca5766b2ee1c7fb51
coddingp <theodore1seriakov@gmail.com>
2021-12-15 01:14:28 +0600

    Add files via upload

---
+++ src/circles/Main.java
@@ -0,0 +1,15 @@
+package circles;
+import javax.swing.*;
+import java.awt.*;
+//import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        Circle redCircle = new Circle(18, "Red");
+
+        System.out.println(redCircle.toString());
+        Circle blueCircle = new Circle(13, "Blue");
+        System.out.println(blueCircle.toString());
+
+    }
+}
+
---
+++ src/circles/Test.java
@@ -0,0 +1,33 @@
+package circles;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Test extends Canvas {
+    String color;
+    public Test(String color) {
+        this.color =color;
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        graphics.setColor(Color.MAGENTA);
+        // Нас интересует только этот блок
+        graphics.fillOval(100, 50, 200, 200); // Остальное магия…
+    }
+
+    public static void main(String[] args){
+        Circle redCircle = new Circle(18, "BLUE");
+        Test canvas  = new Test(redCircle.getColor());
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(canvas);
+        frame.setVisible(true);
+    }
+
+
+
+
+
+}
